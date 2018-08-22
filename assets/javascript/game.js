5df27275fb375dd4f4942e80a8fc5a8a5defee06
lblueter <logan11023@yahoo.com>
2018-08-22 14:25:43 -0400

    building html and style

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+var fighter = {
+    health: "180",
+    damage: "7",
+}
+var cleric = {
+    health: "200",
+    damage: "6"
+}
+var rogue = {
+    health: "150",
+    damage: "10",
+}
+var mage = {
+    health: "120",
+    damage: "20"
+}
+
+var add = function (one, two) {
+    return one + two
+}
+
+$("")
