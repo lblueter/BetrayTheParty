d8cfe517d0298e80dbbddf73effc03e0f8ddd6e5
lblueter <logan11023@yahoo.com>
2018-08-24 00:45:53 -0400

    added portrait images and worked on javascript, mostly done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+var partyRemaining = 3
 var fighter = {
     health: 180,
     damage: 8,
@@ -34,21 +35,48 @@ var mage = {
 // 180
 // 60
 //counter*5=90
-
+$(".fighter-hp").text(fighter.health)
+$(".rogue-hp").text(rogue.health)
+$(".mage-hp").text(mage.health)
+$(".cleric-hp").text(cleric.health)
 var minus = function (one, two) {
     return one - two
 }
-var atkHp
-var atkDamage
-var defendHp
-var defendDmg
-var currentDamage;
+var atkHp = 0
+var atkDamage = 0
+var defendHp = 0
+var defendDmg = 0
+var currentDamage = 0;
 var characterSelected = false;
 var enemySelected = false;
 var selectedChar = ""
 var selectedEnemy = ""
+
+var reset = function () {
+    partyRemaining = 3
+    atkHp = 0
+    atkDamage = 0
+    defendHp = 0
+    defendDmg = 0
+    currentDamage = 0;
+    characterSelected = false;
+    enemySelected = false;
+    selectedChar = ""
+    selectedEnemy = ""
+    $("#charSelect").empty()
+    $("#enemy").empty()
+    $(".char").show()
+    $(".instructions").text("Choose your Character!")
+    $("#youAtk").text("")
+    $("#enemyAtk").text("")
+    $(".fighter-hp").text(fighter.health)
+    $(".rogue-hp").text(rogue.health)
+    $(".mage-hp").text(mage.health)
+    $(".cleric-hp").text(cleric.health)
+}
+
 console.log(characterSelected)
-$(".char").on("click", function () { 
+$(".char").on("click", function () {
     console.log("click works")
     if (characterSelected === false) {
         characterSelected = true;
@@ -104,24 +132,72 @@ $(".char").on("click", function () {
 })
 
 $("#atkBtn").on("click", function () {
-    currentDamage += atkDamage;
+    console.log("atk work?")
+    if (characterSelected === true && enemySelected === true) {
+        currentDamage += atkDamage;
 
-    defendHp = minus(defendHp, currentDamage)
-    if (defendHp <= 0) {
+        defendHp = minus(defendHp, currentDamage)
+        atkHp = minus(atkHp, defendDmg)
+        if (defendHp <= 0) {
+            $("#youAtk").text("You have slain the " + selectedEnemy + ". Select your next target.")
+            $("#enemyAtk").text("")
+            enemySelected = false;
+            $("#enemy").empty()
+            partyRemaining--;
+        }
+        else if (atkHp <= 0) {
+            $("#youAtk").text("You Died. Try again.")
+            $("#enemyAtk").text("")
+        }
+        else {
+            $("#youAtk").text("You hit the " + selectedEnemy + " for " + currentDamage)
+            $("#enemyAtk").text("The " + selectedEnemy + " attacked you for " + defendDmg)
+        }
+
+        if (selectedEnemy === "fighter") {
+            $(".fighter-hp").text(defendHp)
+        }
+        else if (selectedEnemy === "rogue") {
+            $(".rogue-hp").text(defendHp)
+        }
+        else if (selectedEnemy === "mage") {
+            $(".mage-hp").text(defendHp)
+        }
+        else if (selectedEnemy === "cleric") {
+            $(".cleric-hp").text(defendHp)
+        }
+
+        if (selectedChar === "fighter") {
+            $(".fighter-hp").text(atkHp)
+        }
+        else if (selectedChar === "rogue") {
+            $(".rogue-hp").text(atkHp)
+        }
+        else if (selectedChar === "mage") {
+            $(".mage-hp").text(atkHp)
+        }
+        else if (selectedChar === "cleric") {
+            $(".cleric-hp").text(atkHp)
+        }
+
+        $()
+        console.log(atkHp)
+        console.log(currentDamage)
+        console.log(defendHp)
+        console.log(defendDmg)
+
+        console.log(selectedChar)
+        console.log(selectedEnemy)
 
     }
-    atkHp = minus(atkHp, defendDmg)
-    if (atkHp <= 0) {
-
+    if (partyRemaining <= 0) {
+        $("#youAtk").text("You have betrayed the party.")
+        $("#enemyAtk").text("Congratulations.")
     }
-    console.log(atkHp)
-    console.log(defendHp)
-    console.log(defendDmg)
-    console.log(currentDamage)
-    console.log("atk works")
-    console.log(selectedChar)
-    console.log(selectedEnemy)
 })
+
+
 $("#resetBtn").on("click", function () {
     console.log("reset works")
+    return reset()
 })
